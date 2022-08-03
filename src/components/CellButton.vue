4eef99dc9dff64fe70ba05e26284666a9f8f065c
Ma-Vollmer <matthias.vo2107@gmail.com>
2022-08-04 00:37:32 +0200

    update in CellButton is implemented

--- src/components/CellButton.vue
+++ src/components/CellButton.vue
@@ -1,5 +1,5 @@
 <template>
-<img :src="image" alt="" @click="reveal" id="button"/>
+<img :src="image" alt="" @click="update" id="button"/>
 </template>
 <script>
 import { invoke } from '@tauri-apps/api/tauri'
@@ -21,9 +21,17 @@ export default {
         },
         update() {
             console.log("in update!");
-            this.image = require('../assets/bomb.png');
             invoke('get_info', {x: parseInt(this.x), y: parseInt(this.y)}).then((ret)=>{
                 console.log(ret);
+                    })
+            invoke('reveal', {x: parseInt(this.x), y: parseInt(this.y)}).then((message)=>{console.log(message)})
+            invoke('get_info', {x: parseInt(this.x), y: parseInt(this.y)}).then((ret)=>{
+                    switch(ret){
+                        case "unrevealed": this.image = require('../assets/unrevealed.png'); break;
+                        case "bomb": this.image = require('../assets/bomb.png'); break;
+                        case "marked": this.image = require('../assets/marked.png'); break;
+                        default: this.image = require('../assets/number_' + ret + '.png');
+                    }
                     })
         }
     }
